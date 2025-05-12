f8880e091c5a01e91b49c5263a849ff1a7ee1a3c
Zhang Miaochang <superzhangmch@163.com>
2025-05-12 16:17:25 +0800

    Update model.py

--- funasr/models/bicif_paraformer/model.py
+++ funasr/models/bicif_paraformer/model.py
@@ -270,7 +270,15 @@ class BiCifParaformer(Paraformer):
         encoder_out, encoder_out_lens = self.encode(speech, speech_lengths)
         if isinstance(encoder_out, tuple):
             encoder_out = encoder_out[0]
-
+        # print (encoder_out.shape, encoder_out_lens)
+        # torch.Size([3, 84, 512]) tensor([45, 77, 84], device='cuda:0') # note: 84 = max(45, 77, 84)
+        # 上面输出是在加载 modelscope 下的 model：modelscope/hub/iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch/
+        # 对某个 audio【该modelscope model 下的 example/asr_example.wav】作为input的结果。下面都基于此. 原始 input audio 被切分成了3个不等长的片段，内容分别是：
+        # - 因为如果当你认为这个世界没有正义， 
+        # - 正是因为存在绝对正义所以我们接受现实的相对正义
+        # - 但是不要因为>现实的相对正义我们就认为这个世界没有正义
+        # 也就是说，是按自然语音停顿分隔 (VAD) 做的切分
+    
         # predictor
         predictor_outs = self.calc_predictor(encoder_out, encoder_out_lens)
         pre_acoustic_embeds, pre_token_length, alphas, pre_peak_index = (
@@ -282,15 +290,9 @@ class BiCifParaformer(Paraformer):
 
         # print (pre_acoustic_embeds.shape, pre_token_length.shape, alphas.shape, pre_peak_index.shape)
         # 输出： torch.Size([3, 27, 512]) torch.Size([3]) torch.Size([3, 85]) torch.Size([3, 85])
-        # 上面输出是加载 modelscope 下的 model：modelscope/hub/iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch/
-        # 对某个 audio（该modelscope model 下的 example/asr_example.wav），输出如下（注意切分成了 3 个不等长的片段。三个片段是原始语句的三个完整子句，所以应该是背后是靠的 VAD 切分的）
-        # 
-        # 上面的 [3, 27, 512] == (batch_size, seq_len, dim_feature)
-        # 三个子句是：
-        # - 因为如果当你认为这个世界没有正义， 
-        # - 正是因为存在绝对正义所以我们接受现实的相对正义
-        # - 但是不要因为>现实的相对正义我们就认为这个世界没有正义
-        
+        # [3, 27, 512] == (batch_size, seq_len, dim_feature)。 seq_len=27 表示 27 个token。
+        # [3, 85] 中的 85，对应前面的 max([45, 77, 84]) + 1
+
         pre_token_length = pre_token_length.round().long()
         if torch.max(pre_token_length) < 1:
             return []
@@ -298,6 +300,10 @@ class BiCifParaformer(Paraformer):
             encoder_out, encoder_out_lens, pre_acoustic_embeds, pre_token_length
         )
         decoder_out, ys_pad_lens = decoder_outs[0], decoder_outs[1]
+        # print (decoder_out.shape, ys_pad_lens)
+        # torch.Size([3, 27, 8404]) tensor([17, 24, 27], device='cuda:0')
+        # 三个子句分别有 17， 24， 27 个 tokens。8404 是词表大小（model_name=speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch中也有8404）
+        # asr 一个难点就是语音和文字的对齐问题，所以才发明了 CTC。而 funASR 这样的方法巧妙之处就在于预测出了每个token的语音长度，先预测出了有多少个token，从而解决了对齐问题。
 
         # BiCifParaformer, test no bias cif2
         _, _, us_alphas, us_peaks = self.calc_predictor_timestamp(
@@ -307,9 +313,13 @@ class BiCifParaformer(Paraformer):
         results = []
         b, n, d = decoder_out.size()
         for i in range(b):
+            # 遍历每一个子句
             x = encoder_out[i, : encoder_out_lens[i], :]
             am_scores = decoder_out[i, : pre_token_length[i], :]
+
+            # beam search or greedy search
             if self.beam_search is not None:
+                # beam search：注意，如果用到它，则要么是用到了ctc，要么是用到了 ngram 之类语言模型的辅助。否则 funASR 本身的方法是没法用beam search的（因为 greedy search 已经是最优解了）
                 nbest_hyps = self.beam_search(
                     x=x,
                     am_scores=am_scores,
@@ -319,14 +329,16 @@ class BiCifParaformer(Paraformer):
 
                 nbest_hyps = nbest_hyps[: self.nbest]
             else:
-
+                # greedy 搜索。默认分支
                 yseq = am_scores.argmax(dim=-1)
                 score = am_scores.max(dim=-1)[0]
                 score = torch.sum(score, dim=-1)
                 # pad with mask tokens to ensure compatibility with sos/eos tokens
                 yseq = torch.tensor([self.sos] + yseq.tolist() + [self.eos], device=yseq.device)
-                nbest_hyps = [Hypothesis(yseq=yseq, score=score)]
+                nbest_hyps = [Hypothesis(yseq=yseq, score=score)] # Hypothesis并没干啥特别的
+
             for nbest_idx, hyp in enumerate(nbest_hyps):
+                # 注意 hyp 已经有结果 token id 了，只差反解出 text 了
                 ibest_writer = None
                 if kwargs.get("output_dir") is not None:
                     if not hasattr(self, "writer"):
@@ -349,12 +361,12 @@ class BiCifParaformer(Paraformer):
 
                 if tokenizer is not None:
                     # Change integer-ids to tokens
-                    token = tokenizer.ids2tokens(token_int)
-                    text = tokenizer.tokens2text(token)
+                    token = tokenizer.ids2tokens(token_int) # 每个token的text
+                    text = tokenizer.tokens2text(token) # 把 token_arr 拼接成最终 text
 
-                    _, timestamp = ts_prediction_lfr6_standard(
-                        us_alphas[i][: encoder_out_lens[i] * 3],
-                        us_peaks[i][: encoder_out_lens[i] * 3],
+                    _, timestamp = ts_prediction_lfr6_standard(  # 这个函数内部，其实就是：利用 CIF 模型中每个 token 的 fire point（触发点），来界定该 token 在原始音频中的时间偏移（time offset）和持续时间。
+                        us_alphas[i][: encoder_out_lens[i] * 3], # 其实正是从 alphas 推断出的 us_peaks
+                        us_peaks[i][: encoder_out_lens[i] * 3],  # fire 点
                         copy.copy(token),
                         vad_offset=kwargs.get("begin_time", 0),
                     )
